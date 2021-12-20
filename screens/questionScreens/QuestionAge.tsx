6400d88ce8a76c8c63f896a482d43849ca609544
thanh-dev0306 <87410617+thanh-dev0306@users.noreply.github.com>
2021-12-21 02:51:55 +0700

    oh

--- screens/questionScreens/QuestionAge.tsx
+++ screens/questionScreens/QuestionAge.tsx
@@ -1,14 +1,17 @@
 import { styles } from './styles.js';
 import { Image, View, TouchableOpacity, } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WheelPicker from 'react-native-wheely';
 import { Text } from '../../components/Themed';
 
 
 const QuestionAge = ({ navigation, route }: { navigation: NavigationProp<any>, route: any }) => {
     const [selectedIndex, setSelectedIndex] = useState(0);
-    const age = [10, 11, 12, 13, 14, 15]
+    const age = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+    useEffect(() => {
+        console.log(selectedIndex);
+    }, [selectedIndex])
     return (
         <View style={styles.questionWrap}>
             <View style={styles.questionHeader}>
@@ -34,7 +37,7 @@ const QuestionAge = ({ navigation, route }: { navigation: NavigationProp<any>, r
                             }}
                         ></Text>
                         <WheelPicker
-                            selectedIndex={3}
+                            selectedIndex={1}
                             options={age}
                             onChange={(index) => setSelectedIndex(index)}
                             itemTextStyle={styles.wheelTitle}
